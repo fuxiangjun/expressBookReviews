361a38fa25781260416ef2f4ce1056f29e88fe80
Xiangjun <fuxiangjun2013@gmail.com>
2025-04-29 15:08:06 -0400

    update task end'

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -94,4 +94,140 @@ public_users.get('/review/:isbn',function (req, res) {
     return res.status(200).json({ reviews: reviews });
 });
 
+//Task-10-13 common function
+// Function to fetch book list using Promise callbacks
+function getBookListWithPromise(url) {
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => resolve(response.data))
+      .catch(error => reject(error));
+  });
+}
+//Task-10-13 common function
+// Function to fetch book list using async-await
+async function getBookListAsync(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw error; // Re-throw the error for handling in the route
+  }
+}
+
+//Task-10 With Promise
+public_users.get('/promise', function (req, res) {
+  try {
+    getBookListWithPromise('http://localhost:5000/') 
+      .then(bookList => {
+        res.json(bookList);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book list" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+
+//Task-10 With Async
+public_users.get('/async', async function (req, res) {
+  try {
+    const bookList = await getBookListAsync('http://localhost:5000/'); //
+    res.json(bookList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book list" });
+  }
+});
+
+// Task-11 With Promise
+public_users.get('/promise/isbn/:isbn', function (req, res) {
+  try {
+    const requestedIsbn = req.params.isbn;
+    getBookListWithPromise("http://localhost:5000/isbn/" + requestedIsbn) 
+      .then(book => {
+        res.json(book);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book details" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+// Task-11 With Async
+public_users.get('/async/isbn/:isbn', async function (req, res) {
+  try {
+    const requestedIsbn = req.params.isbn;
+    const book = await getBookListAsync("http://localhost:5000/isbn/" + requestedIsbn);
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
+// Task-12 With Promise
+public_users.get('/promise/author/:author', function (req, res) {
+  try {
+    const requestedAuthor = req.params.author;
+    getBookListWithPromise("http://localhost:5000/author/" + requestedAuthor) 
+      .then(book => {
+        res.json(book);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book details" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+// Task-12 With Async
+public_users.get('/async/author/:author', async function (req, res) {
+  try {
+    const requestedAuthor = req.params.author;
+    const book = await getBookListAsync("http://localhost:5000/author/" + requestedAuthor);
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
+// Task-13 With Promise
+public_users.get('/promise/title/:title', function (req, res) {
+  try {
+    const requestedTitle = req.params.title;
+    getBookListWithPromise("http://localhost:5000/title/" + requestedTitle) 
+      .then(book => {
+        res.json(book);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving book details" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unexpected error" });
+  }
+});
+// Task-13 With Async
+public_users.get('/async/title/:title', async function (req, res) {
+  try {
+    const requestedTitle = req.params.title;
+    const book = await getBookListAsync("http://localhost:5000/title/" + requestedTitle);
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
+
 module.exports.general = public_users;
